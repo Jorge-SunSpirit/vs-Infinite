b49c08332ce2d72e369019b6bb8ffa19c47ef682
Sun Spirit <glitchedminer@gmail.com>
2022-09-25 05:25:07 -0400

    Stage, have fun editing if need be

---
+++ example_mods/stages/sunsetheights.lua
@@ -0,0 +1,37 @@
+function onCreate()
+
+	posX = -600;
+	posY = -200;
+	scale = 2;
+	-- background shit
+	makeLuaSprite('bg', 'SSH/skybox', posX, posY);
+	setScrollFactor('bg', 0.2, 1);
+	scaleObject('bg', scale, scale);
+	updateHitbox('bg');
+	
+	makeLuaSprite('stageback', 'SSH/bg', posX, posY);
+	setScrollFactor('stageback', 0.9, 1);
+	scaleObject('stageback', scale, scale);
+	updateHitbox('stageback');
+	
+	makeLuaSprite('ground', 'SSH/main', posX, posY);
+	setScrollFactor('ground', 1, 1);
+	scaleObject('ground', scale, scale);
+	updateHitbox('ground');
+	
+	makeLuaSprite('foreground', 'SSH/foreground', posX, posY);
+	setScrollFactor('foreground', 1.2, 1);
+	scaleObject('foreground', scale, scale);
+	updateHitbox('foreground');
+
+	
+	addLuaSprite('bg', false);
+	addLuaSprite('stageback', false);
+	addLuaSprite('ground', false);
+	addLuaSprite('foreground', true);
+end
+
+function onCreatePost()
+	setProperty('gf.alpha', 0.001);
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end

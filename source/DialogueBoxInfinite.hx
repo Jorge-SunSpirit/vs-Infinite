e625841039d98626c99b9fcc59612358a811daa0
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-10-03 14:59:14 -0700

    init textbox stuff

---
+++ source/DialogueBoxInfinite.hx
@@ -0,0 +1,161 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.text.FlxTypeText;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.FlxKeyManager;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class DialogueBoxInfinite extends FlxSpriteGroup
+{
+	var box:FlxSprite;
+
+	var curCharacter:String = '';
+
+	var dialogueList:Array<String> = [];
+
+	var swagDialogue:FlxTypeText;
+
+	public var finishThing:Void->Void;
+	public var nextDialogueThing:Void->Void = null;
+	public var skipDialogueThing:Void->Void = null;
+
+	var portraitLeft:FlxSprite;
+
+	var bgFade:FlxSprite;
+
+	public function new(talkingRight:Bool = true, ?dialogueList:Array<String>)
+	{
+		super();
+
+		bgFade = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0xFFB3DFd8);
+		bgFade.alpha = 0;
+		add(bgFade);
+
+		new FlxTimer().start(0.83, function(tmr:FlxTimer)
+		{
+			bgFade.alpha += (1 / 5) * 0.7;
+			if (bgFade.alpha > 0.7)
+				bgFade.alpha = 0.7;
+		}, 5);
+
+		box = new FlxSprite(151, 460).loadGraphic(Paths.image('textbox'));
+		box.antialiasing = ClientPrefs.globalAntialiasing;
+		add(box);
+
+		this.dialogueList = dialogueList;
+
+		// bring in code from DDTO
+		portraitLeft = new FlxSprite(-20, 40);
+		portraitLeft.frames = Paths.getSparrowAtlas('weeb/senpaiPortrait');
+		portraitLeft.animation.addByPrefix('enter', 'Senpai Portrait Enter', 24, false);
+		portraitLeft.setGraphicSize(Std.int(portraitLeft.width * PlayState.daPixelZoom * 0.9));
+		portraitLeft.updateHitbox();
+		portraitLeft.scrollFactor.set();
+		add(portraitLeft);
+		portraitLeft.visible = false;
+
+		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
+		swagDialogue.font = Paths.font("futura.otf");
+		swagDialogue.color = 0xFF3F2021;
+		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
+		add(swagDialogue);
+	}
+
+	var dialogueOpened:Bool = false;
+	var dialogueStarted:Bool = false;
+	var dialogueEnded:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (dialogueOpened && !dialogueStarted)
+		{
+			startDialogue();
+			dialogueStarted = true;
+		}
+
+		if(PlayerSettings.player1.controls.ACCEPT)
+		{
+			if (dialogueEnded)
+			{
+				remove(dialogue);
+				if (dialogueList[1] == null && dialogueList[0] != null)
+				{
+					if (!isEnding)
+					{
+						isEnding = true;
+						FlxG.sound.play(Paths.sound('clickText'), 0.8);	
+
+						if (PlayState.SONG.song.toLowerCase() == 'senpai' || PlayState.SONG.song.toLowerCase() == 'thorns')
+							FlxG.sound.music.fadeOut(1.5, 0);
+
+						new FlxTimer().start(0.2, function(tmr:FlxTimer)
+						{
+							box.alpha -= 1 / 5;
+							bgFade.alpha -= 1 / 5 * 0.7;
+							portraitLeft.visible = false;
+							swagDialogue.alpha -= 1 / 5;
+						}, 5);
+
+						new FlxTimer().start(1.5, function(tmr:FlxTimer)
+						{
+							finishThing();
+							kill();
+						});
+					}
+				}
+				else
+				{
+					dialogueList.remove(dialogueList[0]);
+					startDialogue();
+					FlxG.sound.play(Paths.sound('clickText'), 0.8);
+				}
+			}
+			else if (dialogueStarted)
+			{
+				FlxG.sound.play(Paths.sound('clickText'), 0.8);
+				swagDialogue.skip();
+				
+				if(skipDialogueThing != null) {
+					skipDialogueThing();
+				}
+			}
+		}
+		
+		super.update(elapsed);
+	}
+
+	var isEnding:Bool = false;
+
+	function startDialogue():Void
+	{
+		cleanDialog();
+
+		swagDialogue.resetText(dialogueList[0]);
+		swagDialogue.start(0.04, true);
+		swagDialogue.completeCallback = function() {
+			dialogueEnded = true;
+		};
+
+		dialogueEnded = false;
+
+		// bring in some dialogue code from DDTO
+
+		if(nextDialogueThing != null) {
+			nextDialogueThing();
+		}
+	}
+
+	function cleanDialog():Void
+	{
+		var splitName:Array<String> = dialogueList[0].split(":");
+		curCharacter = splitName[1];
+		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+	}
+}

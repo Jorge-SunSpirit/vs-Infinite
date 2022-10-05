df1a08524bbc0ac22f285fe67d4d1edef1252406
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-10-05 15:36:19 -0700

    credit since we have gpu texutres

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -94,7 +94,7 @@ class CreditsState extends MusicBeatState
 			['SqirraRNG',			'gedehari',			'Chart Editor\'s Sound Waveform base',						'https://twitter.com/gedehari',			'FF9300'],
 			['PolybiusProxy',		'polybiusproxy',	'.MP4 Video Loader Extension',								'https://twitter.com/polybiusproxy',	'FFEAA6'],
 			['Keoiki',				'keoiki',			'Note Splash Animations',									'https://twitter.com/Keoiki_',			'FFFFFF'],
-			['Smokey',				'smokey',			'Spritemap Texture Support',								'https://twitter.com/Smokey_5_',		'4D5DBD'],
+			['Smokey',				'smokey',			'Spritemap Texture Support, GPU Textures',								'https://twitter.com/Smokey_5_',		'4D5DBD'],
 			[''],
 			["Funkin' Crew"],
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",						'https://twitter.com/ninja_muffin99',	'F73838'],

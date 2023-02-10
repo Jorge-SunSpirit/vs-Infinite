031b934e4becfa1dd76b59b9acd3a36e6fab4371
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-02-10 11:34:11 -0800

    actually this would save space instead

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -112,7 +112,7 @@ class VisualsUISubState extends BaseOptionsMenu
 			'pauseMusic',
 			'string',
 			'Frozen Projections',
-			['None', /*'Breakfast', 'Tea Time',*/ 'Frozen Projections']);
+			['None', 'Frozen Projections']);
 		addOption(option);
 		option.onChange = onChangePauseMusic;
 		

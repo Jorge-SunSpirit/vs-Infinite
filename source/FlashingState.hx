ac1b480f4a765a67baf60b52085a11a63be3c7f4
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-11-14 19:36:02 -0800

    discalimer

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -2,66 +2,69 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.FlxSubState;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
-import flixel.effects.FlxFlicker;
-import lime.app.Application;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.tweens.FlxTween;
-import flixel.util.FlxTimer;
 
 class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
-	var warnText:FlxText;
+	var disclaimer:FlxSprite;
+
 	override function create()
 	{
 		super.create();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
-
-		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
-			32);
-		warnText.setFormat(Paths.font("futura.otf"), 32, FlxColor.WHITE, CENTER);
-		warnText.screenCenter(Y);
-		add(warnText);
+		disclaimer = new FlxSprite().loadGraphic(Paths.image('VSInfiniteWARNING'));
+		disclaimer.antialiasing = ClientPrefs.globalAntialiasing;
+		disclaimer.setGraphicSize(FlxG.width, FlxG.height);
+		disclaimer.screenCenter();
+		add(disclaimer);
 	}
 
 	override function update(elapsed:Float)
 	{
-		if(!leftState) {
+		if (!leftState)
+		{
 			var back:Bool = controls.BACK;
-			if (controls.ACCEPT || back) {
+
+			if (controls.ACCEPT || back)
+			{
 				leftState = true;
+
 				FlxTransitionableState.skipNextTransIn = true;
 				FlxTransitionableState.skipNextTransOut = true;
-				if(!back) {
+
+				if (!back)
+				{
 					ClientPrefs.flashing = false;
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
-					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+
+					// WHY DID IT FLASH WHEN YOU TELL IT TO NOT FLASH LMAO
+					FlxTween.tween(disclaimer, {alpha: 0}, 1.5,
+					{
+						onComplete: function (twn:FlxTween)
+						{
 							MusicBeatState.switchState(new TitleState());
-						});
+						}
 					});
-				} else {
-					FlxG.sound.play(Paths.sound('cancelMenu'));
-					FlxTween.tween(warnText, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
+				}
+				else
+				{
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+
+					FlxTween.tween(disclaimer, {alpha: 0}, 1.5,
+					{
+						onComplete: function (twn:FlxTween)
+						{
 							MusicBeatState.switchState(new TitleState());
 						}
 					});
 				}
 			}
 		}
+
 		super.update(elapsed);
 	}
 }

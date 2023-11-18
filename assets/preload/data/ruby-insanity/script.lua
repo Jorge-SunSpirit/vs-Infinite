d87c4d1e0d0944c143ff7ae91d58679fd186cc8f
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-11-17 20:26:35 -0800

    center camera during dialogue

--- assets/preload/data/ruby-insanity/script.lua
+++ assets/preload/data/ruby-insanity/script.lua
@@ -1,9 +1,13 @@
 function onStepHit()
 	if curStep == 4480 then
 		doTweenAlpha('hudAlpha', 'camHUD', 0, 0.4, 'linear');
+		setProperty('cameraSpeed', 2);
+		triggerEvent('Camera Follow Pos', '620', '326');
 	end
 
 	if curStep == 4992 then
 		setProperty('camHUD.alpha', 1);
+		setProperty('cameraSpeed', 1);
+		triggerEvent('Camera Follow Pos', '', '');
 	end
 end

95773ab65836a2374d586acdaeaf1f9afb155b65
Sun Spirit <glitchedminer@gmail.com>
2023-11-26 23:48:14 -0500

    >:(

--- assets/preload/stages/sunsetheights.lua
+++ assets/preload/stages/sunsetheights.lua
@@ -42,7 +42,3 @@ function onCreate()
 	addLuaSprite('ground', false);
 	addLuaSprite('foreground', true);
 end
-
-function onCreatePost()
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end

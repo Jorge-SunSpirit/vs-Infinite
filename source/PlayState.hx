571ccff16773e650ff6a63c1223b4bab2c5b5132
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-11-29 15:08:28 -0800

    why are these on camOther2

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1374,7 +1374,7 @@ class PlayState extends MusicBeatState
 			}
 			infiniteDialogue.nextDialogueThing = startNextDialogue;
 			infiniteDialogue.skipDialogueThing = skipDialogue;
-			infiniteDialogue.cameras = [camOther2];
+			infiniteDialogue.cameras = [camOther];
 			add(infiniteDialogue);
 		}
 		else
@@ -1404,7 +1404,7 @@ class PlayState extends MusicBeatState
 				inCutscene = false;
 				camHUD.visible = true;
 			}
-			fakeInfiniteDialogue.cameras = [camOther2];
+			fakeInfiniteDialogue.cameras = [camOther];
 			add(fakeInfiniteDialogue);
 		}
 		else
